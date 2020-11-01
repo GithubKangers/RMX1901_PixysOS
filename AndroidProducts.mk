b6f284d09f5340c07abe69f7b625a9bc766b53f5
icecrac34r <66408922+icecrac34r@users.noreply.github.com>
2020-11-01 20:30:15 +0530

    DT

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pixys_RMX1901.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixys_RMX1901-userdebug \
+    pixys_RMX1901-eng
